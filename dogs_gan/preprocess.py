35270a48360da2b7c71031d70438061661298f7c
thecosta <bru.costa.bc@gmail.com>
2019-08-02 14:20:56 -0400

    Batch sized changed

--- dogs_gan/preprocess.py
+++ dogs_gan/preprocess.py
@@ -6,17 +6,19 @@ from tqdm import tqdm
 
 
 
+path_in = '/Users/bruno/Documents/git/burncam/data/images/'
+path_out = '/Users/bruno/Documents/git/burncam/data/images_gen/'
 train_datagen = ImageDataGenerator(rescale=1.0/255)
 test_datagen = ImageDataGenerator(rescale=1.0/255)
 
 train_generator = train_datagen.flow_from_directory(
-    '/Volumes/Brome/kaggle/data/all-dogs/',
-    target_size=(64, 64),
-    batch_size=32,
-    save_to_dir='/Volumes/Brome/kaggle/data/all-dogs-gen/',
+    path_in,
+    target_size=(512, 512),
+    batch_size=221,
+    save_to_dir=path_out,
     save_prefix='aug',
     save_format='png',
     class_mode=None)
 
-for i in tqdm(train_generator, total=20579):
-    continue
+for i in train_generator:
+    break
